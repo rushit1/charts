2b58d9653da267f311308bdce94c2de4e295927b
rushit1 <69600052+rushit1@users.noreply.github.com>
2020-08-16 09:54:08 -0400

    Create c66.js

---
+++ c66.js
@@ -0,0 +1,89 @@
+looker.plugins.visualizations.add({
+create: function(element, config) {
+  element.innerHTML = `
+	<style>
+	.sannith {
+	min-width: 310px;
+	height: 400px
+	}
+	</style>
+	`;
+	
+    var container = element.appendChild(document.createElement("div"));
+	container.className = "sannith";
+    container.id = 'container';
+		
+  },
+
+updateAsync: function(data, element, config, queryResponse, details, doneRendering) {
+// Clear any errors from previous updates:
+this.clearErrors();
+
+// Dump data and metadata to console:
+console.log('updateAsync() data', data)
+console.log('updateAsync() config', config)
+console.log('updateAsync() queryResponse', queryResponse)
+	
+	
+
+// get the names of the first dimension and measure available in data
+x = config.query_fields.dimensions[0].name;
+y = config.query_fields.dimensions[1].name;
+z1 = config.query_fields.dimensions[2].name;
+z2 = config.query_fields.dimensions[3].name;
+z3 = config.query_fields.dimensions[4].name;
+
+	
+// build data array for the chart, by iterating over the Looker data object
+var lnData = [];
+
+for(var row of data) {
+	var cell = row[queryResponse.fields.dimensions[0].name]
+	lnData.push([
+		row[x].value,
+		row[y].value,
+		row[z1].value,
+		row[z2].value,
+		row[z3].value
+	]);
+	
+}
+	
+console.log('Chart data', lnData)
+Highcharts.stockChart('container', {
+
+
+        title: {
+            text: 'AAPL stock price by minute'
+        },
+
+        rangeSelector: {
+            buttons: [{
+                type: 'hour',
+                count: 1,
+                text: '1h'
+            }, {
+                type: 'day',
+                count: 1,
+                text: '1D'
+            }, {
+                type: 'all',
+                count: 1,
+                text: 'All'
+            }],
+            selected: 1,
+            inputEnabled: false
+        },
+
+        series: [{
+            name: 'AAPL',
+            type: 'candlestick',
+            data: lnData,
+            tooltip: {
+                valueDecimals: 2
+            }
+        }]
+    }) 
+doneRendering();
+}
+})
