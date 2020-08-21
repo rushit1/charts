bcb1f3013954ff114ffd4dd4a1ca928502462771
rushit1 <69600052+rushit1@users.noreply.github.com>
2020-08-21 11:33:03 -0400

    Create candle_stick_line.js

---
+++ candle_stick_line.js
@@ -0,0 +1,150 @@
+looker.plugins.visualizations.add({
+create: function(element, config) {
+  element.innerHTML = `
+	<style>
+	.sannith {
+	min-width: 310px;
+	height: 400px
+	}
+	</style>
+	`;
+	
+    var container = element.appendChild(document.createElement("div"));
+	container.className = "sannith";
+    container.id = 'container';
+		
+  },
+
+updateAsync: function(data, element, config, queryResponse, details, doneRendering) {
+// Clear any errors from previous updates:
+this.clearErrors();
+
+// Dump data and metadata to console:
+console.log('updateAsync() data', data)
+console.log('updateAsync() config', config)
+console.log('updateAsync() queryResponse', queryResponse)
+	
+	
+
+// get the names of the first dimension and measure available in data
+
+x = config.query_fields.dimensions[0].name;     // Date
+y = config.query_fields.dimensions[1].name;     // Open
+z1 = config.query_fields.dimensions[2].name;    // High
+z2 = config.query_fields.dimensions[3].name;    // Low 
+z3 = config.query_fields.dimensions[4].name;    // Close
+z5 = config.query_fields.dimensions[5].name;    // Symbol
+z6 = config.query_fields.dimensions[6].name;    // Last Entry Date
+z7 = config.query_fields.dimensions[7].name;    // Entry Price
+z8 = config.query_fields.dimensions[8].name;    // Target Price 1
+z9 = config.query_fields.dimensions[9].name;   // Target Price 2
+z10 = config.query_fields.dimensions[10].name;  // Target Price 3
+z11 = config.query_fields.dimensions[11].name;  // Stop Loss Price 1
+z12 = config.query_fields.dimensions[12].name;  // Stop Loss Price 2
+z13 = config.query_fields.dimensions[13].name;  // Stop Loss Price 3
+
+// load band variables 
+
+var t1 = row[z8].value
+var t2 = row[z9].value
+var t3 = row[z10].value
+
+var sl1 = row[z11].value
+var sl2 = row[z12].value
+var sl3 = row[z13].value
+	
+// build data array for the chart, by iterating over the Looker data object
+var lnData = [];
+for(var row of data) {
+	var cell = row[queryResponse.fields.dimensions[0].name]
+	lnData.push([
+		row[x].value,
+		row[y].value,
+		row[z1].value,
+		row[z2].value,
+		row[z3].value
+	]);
+	}
+
+var StateData = [];	
+for(var row of data) {
+	
+	if (row[z4]['value'] !== null) 
+	{var cell = row[queryResponse.fields.dimensions[0].name]
+	StateData.push({
+		x: row[x].value,
+		title: row[z4].value,
+		text: row[z4].value
+	});}
+	}
+	
+var symbol       = row[z5].value
+var chart_title  = symbol + ' stock price'
+console.log('Chart data 1', cell)	
+console.log('Chart data', lnData)
+console.log('Chart data 2', StateData)
+Highcharts.stockChart('container', {
+
+
+        title: {
+            text:chart_title
+        },
+
+        rangeSelector: {
+            buttons: [{
+                type: 'hour',
+                count: 1,
+                text: '1h'
+            }, {
+                type: 'day',
+                count: 1,
+                text: '1D'
+            }, {
+                type: 'all',
+                count: 1,
+                text: 'All'
+            }],
+            selected: 1,
+            inputEnabled: false
+        },
+
+        series: [{
+            name: symbol,
+            type: 'candlestick',
+            data: lnData,
+	        id: 'stockseries',
+            tooltip: {
+                valueDecimals: 2
+                     }
+                },
+	            {
+           type: 'flags',
+           data: StateData,
+	       onSeries:'stockseries'	
+                }],
+        yAxis: {
+            plotBands: [{
+                    color: '#32CD32',
+                       to: t2,
+                     from: t1
+                        },
+                        {
+                    color: '#6FDC6F',,
+                      to: t3,
+                    from: t2
+                        },
+						{
+                    color: '#DE2008',
+                       to: sl2,
+                     from: sl1 
+                        },
+                        {
+                    color: '#FF6347',
+                       to: sl3,
+                     from: sl2
+                        }]
+               } 
+    }) 
+doneRendering();
+}
+})
